1718c3e136107c823fd5aa027b108d25c2763ee5
Matheus <matheus.brgsz@gmail.com>
2025-02-11 18:25:40 -0300

    Exploring Views and URLs

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('home')
+
+def my_view(request):
+    return HttpResponse('Hello world')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',  home),
+    path('blog/',  my_view),
 ]
